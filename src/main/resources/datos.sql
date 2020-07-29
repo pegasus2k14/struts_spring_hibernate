81e49a67bb160f9d7735d676cf19c11c9365fca9
Miguel_R <samuraiequis@yahoo.com>
2020-07-28 20:02:25 -0600

    Agregado esquema de BD

---
+++ src/main/resources/datos.sql
@@ -0,0 +1,3 @@
+insert into persona(nombre,ape_paterno,ape_materno,email) values('Admin','admin','admin@cursos.net');
+insert into persona(nombre,ape_paterno,ape_materno,email) values('Juan','Perez','juean@gmail.com');
+insert into persona(nombre,ape_paterno,ape_materno,email) values('Juan','Turcios','jturcios@gmail.com');
